9d22e11936feacb4eeb26e68ca88217747f82d75
Nick <115846324+Atlatl1807@users.noreply.github.com>
2023-09-12 17:16:34 -0400

    initial edit

--- lovelive_song_parser.py
+++ lovelive_song_parser.py
@@ -7,6 +7,7 @@ import aiohttp
 import json
 import asyncio
 import locale
+import requests
 
 from bs4 import BeautifulSoup as bs
 from local import local
@@ -26,7 +27,7 @@ class LLSongs:
 
     # Set variables
     def __init__(self):
-        self.__URL_FANDOM = 'https://love-live.fandom.com/api.php'     # Change links to Fandom
+        self.__URL_FANDOM = 'https://aesthetics.fandom.com/wiki/'     # Change links to Fandom
         self.__URL_GROUPS = [                                          # if they have changed or
             ['Love_Live!', 'text_m'],                                  # new ones have been added.
             ['Love_Live!_Sunshine!!', 'text_aq'],
@@ -37,7 +38,7 @@ class LLSongs:
         self.__amount_songs = 0
         self.__amount_songs_downloaded = 0
         self.__amount_error_songs = 0
-        self.LL_FILE = 'lovelive_songs_list.json' # It can be changed.
+        self.LL_FILE = 'aesthetics_list.json' # It can be changed.
         self.CHUNK_SIZE = 8192 # Download chunk size
 
     def load_songs_list(self):
@@ -50,35 +51,63 @@ class LLSongs:
         except:
             return False
 
-    async def __parse_song(self, song, group, session):
-        async with session.get(self.__URL_FANDOM + '?page=' + song, params={'action' : 'parse', 'format' : 'json'}) as resp:
-            result = await resp.json()
-        result = bs(result['parse']['text']['*'], 'html.parser')
+    async def __parse_aesthetic(self, aesthetic, session):
+        response = requests.get(self.__URL_FANDOM + aesthetic)
+        if response.status_code != 200:
+            print("Error fetching page")
+            exit()
+        else:
+            content = response.content
+        result = bs(content, 'html.parser')
+        print('parsed' + aesthetic)
         if 'Redirect to' in str(result):
-            return await self.__parse_song(result.find('a').attrs['href'][6:], group, session)
-        for source in result.find_all('source'):
-            song_result = [
-                        parse.unquote(source.attrs['src'].split('/')[-3].replace('_', ' ')),
-                        '/'.join(source.attrs['src'].split('/')[:-2])
-            ]
-            self.__songs_list[group].append(song_result)
-            self.__amount_songs += 1
-            PR.print_progress(
-                    get_l('received_song') + \
-                    ' [' + str(self.__amount_songs) + ']: ' + \
-                    song_result[0])
+            return await self.__parse_aesthetic(result.find('a').attrs['href'][6:], session)
+        self.__amount_songs += 1
+        #related_aesthetic = [
+        #            parse.unquote(source.attrs['src'].split('/')[-3].replace('_', ' ')),
+        #            '/'.join(source.attrs['src'].split('/')[:-2])
+        #    ]
+        relateddiv = result.find('div', attrs={'data-source' : 'related_aesthetics'})
+        related_aesthetics = []
+        for a in relateddiv.find_all('a'):
+            try:
+                if 'wiki' in a.attrs['href']:
+                        related_aesthetic = a.attrs['href'][6:]
+                        related_aesthetics.append(related_aesthetic)
+                        PR.print_progress(
+                            'Aesthetic' + \
+                            ' [' + str(self.__amount_songs) + ']: ' + \
+                            aesthetic[0] + ' has related aesthetic: ' +  related_aesthetic)
+            except KeyError:
+                pass
+        print('appending ' + aesthetic + 'with related aesthetics: ' + '.'.join(related_aesthetics))
+        self.__songs_list.append({
+            "aesthetic": aesthetic,
+            "related_aesthetics": related_aesthetics
+        })
 
-    async def __parse_list(self, group, session):
-        async with session.get(self.__URL_FANDOM + '?page=' + group[0], params={'action' : 'parse', 'format' : 'json'}) as resp:
-            result = await resp.json()
-        result = bs(result['parse']['text']['*'], 'html.parser').find_all('div', attrs={'class' : group[1]})
-        self.__songs_list[group[0].replace('_', ' ')] = []
+    
+            
+
+    async def __parse_list(self,session):
+        response = requests.get(self.__URL_FANDOM + 'List_of_Aesthetics')
+        if response.status_code != 200:
+            print("Error fetching page")
+            exit()
+        else:
+            content = response.content
+        result = bs(content, 'html.parser')
+        print('parsed list')
+        twocolumns = result.find_all('div', attrs={'class' : 'twocolumn'})
+        self.__songs_list = []
         tasks = []
-        for i in result:
+        for i in twocolumns:
             for a in i.find_all('a'):
                 try:
                     if 'wiki' in a.attrs['href']:
-                        tasks.append(loop.create_task(self.__parse_song(a.attrs['href'][6:], group[0].replace('_', ' '), session)))
+                        aesthetic = a.attrs['href'][6:]
+                        tasks.append(loop.create_task(self.__parse_aesthetic(aesthetic, session)))
+                        print('attempting to parse' + aesthetic)
                 except KeyError:
                     pass
         await asyncio.wait(tasks)
@@ -105,13 +134,13 @@ class LLSongs:
                     error_text + ']: ' + song_info[0]
         )
 
-    # Updates songs database
+    # Updates aesthetics database
     async def update_songs_list(self):
         self.__songs_list = {}
         self.__amount_songs = 0
         print(get_l('updating_songs'))
         async with aiohttp.ClientSession() as session:
-            await asyncio.wait([loop.create_task(self.__parse_list(group, session)) for group in self.__URL_GROUPS])
+            await asyncio.wait([loop.create_task(self.__parse_list(session))])
         PR.print_last_progress(get_l('update_completed') + ' ' + str(self.__amount_songs))
         print(get_l('writing_to_file'))
         json.dump(self.__songs_list, open(self.LL_FILE, 'w'), indent=4)
@@ -213,15 +242,15 @@ async def main():
         print(get_l('no_database'))
         songs_list = await LL.update_songs_list()
     groups = choice_options(sorted(list(songs_list.keys())), get_l('choose_groups'))
-    while True:
-        parallel = input(get_l('choose_parallel') + ' (' + get_l('default') + ' ' + str(DEFAULT_PARALLEL) + ' - ' + get_l('not_recommend_parallel').upper() + '): ')
-        if not parallel:
-            parallel = DEFAULT_PARALLEL
-            break
-        if re.fullmatch(r'^[1-9](?:\d+)?$', parallel):
-            parallel = int(parallel)
-            break
-    await LL.download_songs(groups, parallel)
+    #while True:
+    #    parallel = input(get_l('choose_parallel') + ' (' + get_l('default') + ' ' + str(DEFAULT_PARALLEL) + ' - ' + get_l('not_recommend_parallel').upper() + '): ')
+    #    if not parallel:
+    #        parallel = DEFAULT_PARALLEL
+    #        break
+    #    if re.fullmatch(r'^[1-9](?:\d+)?$', parallel):
+    #        parallel = int(parallel)
+    #        break
+    #await LL.download_songs(groups, parallel)
 
 async def close():
     for task in asyncio.tasks.all_tasks(loop):
